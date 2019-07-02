5ecc7bf8e45a089a132652010f48741212dd6150
zhouwx <zhouwx@cpchain.io>
2019-07-02 20:33:13 +0800

    ledger first address path should be 44'/337'/0'/0

--- examples/signMessage.py
+++ examples/signMessage.py
@@ -56,7 +56,7 @@ args = parser.parse_args()
 args.message = args.message.encode()
 
 if args.path == None:
-    args.path = "44'/337'"
+    args.path = "44'/337'/0'/0"
 
 encodedTx = struct.pack(">I", len(args.message))
 encodedTx += args.message
